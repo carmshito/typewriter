471482777de4efa363f97276f8009941cde25104
Carmelia Shito <carmshito@hotmail.com>
2023-05-13 21:32:39 -0400

    refactored function to a function expression

--- typewriter.js
+++ typewriter.js
@@ -1,6 +1,6 @@
 const sentence = "hello there from lighthouse labs\n";
 
-function printSentence() {
+const printSentence = function() {
   for (let i = 0; i < sentence.length; i++) {
     setTimeout(() => {
       process.stdout.write(sentence[i]);
